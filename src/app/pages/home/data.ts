c83ea7ae3ee882be16ab0c99d666be73647f79de
sefogi <sebast18@gmail.com>
2025-02-04 23:30:10 +0100

    cambio de copys

--- src/app/pages/home/data.ts
+++ src/app/pages/home/data.ts
@@ -145,15 +145,15 @@ export const features: Feature[] = [
         icon: '',
         name: '',
         shapes: ['shape1', 'shape2'],
-        title: 'Vista general',
-        description: 'La plataforma perfecta para transformar y simplificar la gestión de tu negocio.',
+        title: 'Centralizamos la gestión de sus propiedades en un unico panel ',
+        description: 'Gestión, cobros automaticos y más en un primer vistazo.',
         image: 'assets/images/features/app3.png',
     },
     {
         icon: 'layout',
         name: '',
         shapes: ['shape3', 'shape4'],
-        title: "Planing y Reservas",
+        title: "Disponibilidad y Reservas",
         description: 'Visualiza tu agenda de reservas de forma clara y organizada, para que nunca pierdas de vista tu disponibilidad.',
         image: 'assets/images/features/app4.png'
     },
@@ -161,8 +161,8 @@ export const features: Feature[] = [
         icon: '',
         name: '',
         shapes: ['shape5', 'shape6'],
-        title: 'Tarifas e Inventario',
-        description: "Lleva un control detallado de tus productos o servicios, evitando faltantes y sobrecargas.",
+        title: 'Tarifas dinámicas con precios óptimos según la demanda local',
+        description: "Reglas de cobro personalizadas y automaticas, configura precios dinámicos diarios en tiempo real.",
         image: 'assets/images/features/app4.png',
     }
     
